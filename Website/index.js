c4377347ef86ab3f3ccb4fa43c5135eb1f627a10
Gregory Blood <gregoryblood1998@gmail.com>
2019-10-04 14:52:07 -0700

    Even More Mobile Friendly

--- Website/index.js
+++ Website/index.js
@@ -1,6 +1,23 @@
+var isMobile = false;
 function onAwake() {
     if(/Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        document.getElementById("sections").style.margin = "0% 2%";
+        isMobile = true;
+        document.getElementById("sections").style.margin = "0% 5%";
+
     }
 }
+/*
+function scrollFunction() {
+    if (true) {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            document.getElementById("header").style.visibility = "hidden";
+          } else {
+            document.getElementById("header").style.visibility = "visible";
+        }
+    }   
+}
+window.onscroll = function() {scrollFunction()}; */
+
+
+
 onAwake();
