05584ab4f31b2f84038dfbd8d522bffa0386c5bf
Gregory Blood <gregoryblood1998@gmail.com>
2019-10-07 14:51:05 -0700

    MOBILE FRIENDLIEREST

--- Website/index.js
+++ Website/index.js
@@ -2,23 +2,14 @@ var isMobile = false;
 
 
 function onAwake() {
+    onResize();
     if(/Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         isMobile = true;
-        onResize();
+        
         document.getElementById("sections").style.margin = "0% 5%";
     }
 }
-/*
-function scrollFunction() {
-    if (true) {
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            document.getElementById("header").style.visibility = "hidden";
-          } else {
-            document.getElementById("header").style.visibility = "visible";
-        }
-    }   
-}
-window.onscroll = function() {scrollFunction()}; */
+
 function onResize(){
     if (window.innerWidth <= 630) {
         document.getElementById("sections").style.minWidth = "0px";
