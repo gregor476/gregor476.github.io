1906832256751e72f3c8a467f79e4d9dab3e28c3
Gregory Blood <gregoryblood1998@gmail.com>
2019-12-20 23:08:44 -0800

    Added Text to justdogs

--- justdogpics/script.js
+++ justdogpics/script.js
@@ -2,6 +2,10 @@ var image = document.querySelector(".image");
 
 getpic();
 
+if(/Android|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    isMobile = true;
+    document.getElementById("text").innerHTML = "Tap for more Dogs!";
+}
 
 window.addEventListener('click', () => {
     getpic();
