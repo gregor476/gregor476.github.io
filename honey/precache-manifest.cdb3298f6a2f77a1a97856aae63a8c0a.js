ba5edafb278b314e51409d455383568bcd04aa27
Gregory Blood <gregoryblood1998@gmail.com>
2020-05-19 10:35:13 -0700

    added honey site

---
+++ honey/precache-manifest.cdb3298f6a2f77a1a97856aae63a8c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d410b6908eb530884692dac468b6c6",
+    "url": "/honey/index.html"
+  },
+  {
+    "revision": "97f50eae82a6d35daaf6",
+    "url": "/honey/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c7639ccb2cacd73f4587",
+    "url": "/honey/static/js/2.c02c4cc2.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/honey/static/js/2.c02c4cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f50eae82a6d35daaf6",
+    "url": "/honey/static/js/main.b2b8bdca.chunk.js"
+  },
+  {
+    "revision": "e7fc75608dc7b1e8ccf2",
+    "url": "/honey/static/js/runtime-main.9e0dfc2a.js"
+  },
+  {
+    "revision": "2a642c4801192eea544c2482e4e608d2",
+    "url": "/honey/static/media/honeyjar.2a642c48.png"
+  }
+]);
