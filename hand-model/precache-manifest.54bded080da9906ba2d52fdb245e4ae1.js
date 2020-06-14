98121e2b8e2615f0a31daf455d0bcfee61908f2e
Gregory Blood <gregoryblood1998@gmail.com>
2020-06-14 14:34:00 -0700

    added build

---
+++ hand-model/precache-manifest.54bded080da9906ba2d52fdb245e4ae1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2a16e6423eb04eb00bc7a2396e3c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91ce4bec03564e03f5e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2749d516a94be151f91c",
+    "url": "/static/js/2.6116b13a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6116b13a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ce4bec03564e03f5e2",
+    "url": "/static/js/main.8adf5880.chunk.js"
+  },
+  {
+    "revision": "e7c8710c28de8f83f087",
+    "url": "/static/js/runtime-main.ff01dd2d.js"
+  },
+  {
+    "revision": "aad226dea36c6a6eb9e1f7f098e0382b",
+    "url": "/static/media/img1.aad226de.jpeg"
+  }
+]);
