85921581c2fab8a3dc3c13a939ce80156833949a
Gregory Blood <gregoryblood1998@gmail.com>
2020-06-17 01:41:31 -0700

    Added news app

---
+++ newsforyou/precache-manifest.01ef84d7de9c43910f7b491dfa446528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fea70810b66eec4ad942a2e1292db62",
+    "url": "/newsforyou/index.html"
+  },
+  {
+    "revision": "bf3a8316d4bd8a1eb762",
+    "url": "/newsforyou/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ec8da5d000c433a82c6",
+    "url": "/newsforyou/static/js/2.3a5fbfdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newsforyou/static/js/2.3a5fbfdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3a8316d4bd8a1eb762",
+    "url": "/newsforyou/static/js/main.663411e0.chunk.js"
+  },
+  {
+    "revision": "f983d32806aa09ceaa3b",
+    "url": "/newsforyou/static/js/runtime-main.ba114770.js"
+  }
+]);
