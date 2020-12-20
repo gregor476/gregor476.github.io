7581920910785c82f251fe965623b49117e8bc48
Gregory Blood <gregoryblood1998@gmail.com>
2020-12-20 00:47:30 -0800

    test build

---
+++ portfolio/build/precache-manifest.33ed5098461845e5f8dda929abb2b1f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4f69b17cda7009fbe7680dd02668ac",
+    "url": "/portfolio/build/index.html"
+  },
+  {
+    "revision": "aad74c6db703736dd579",
+    "url": "/portfolio/build/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "41211c513688f62aa189",
+    "url": "/portfolio/build/static/js/2.1d7b8895.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/build/static/js/2.1d7b8895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad74c6db703736dd579",
+    "url": "/portfolio/build/static/js/main.9849e979.chunk.js"
+  },
+  {
+    "revision": "6394d12b9b3bbf310d69",
+    "url": "/portfolio/build/static/js/runtime-main.aa50d5a3.js"
+  },
+  {
+    "revision": "3b8d9c940a349be8ae4504dc0b175846",
+    "url": "/portfolio/build/static/media/ships.3b8d9c94.png"
+  },
+  {
+    "revision": "14c86f325501992c6dbbd875e324898c",
+    "url": "/portfolio/build/static/media/whattowear.14c86f32.png"
+  }
+]);
