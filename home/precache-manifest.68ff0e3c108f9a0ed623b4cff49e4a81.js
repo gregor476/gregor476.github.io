36beb04acd829137716a47670c1b4f39bfcf8dc2
Gregory Blood <gregoryblood1998@gmail.com>
2021-01-15 10:38:49 -0800

    fixed spelling

---
+++ home/precache-manifest.68ff0e3c108f9a0ed623b4cff49e4a81.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd7f4ab432db3db45e8a5e1374f3653",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "ffe77f905cfc54c19bfc",
+    "url": "/home/static/css/main.e46a956e.chunk.css"
+  },
+  {
+    "revision": "12779a4dc2cff8633443",
+    "url": "/home/static/js/2.8e543abf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.8e543abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe77f905cfc54c19bfc",
+    "url": "/home/static/js/main.404e4067.chunk.js"
+  },
+  {
+    "revision": "8489a131ce1d8fd282da",
+    "url": "/home/static/js/runtime-main.56137043.js"
+  },
+  {
+    "revision": "95887a8ce7ceee465daccfc85926b937",
+    "url": "/home/static/media/capstone.95887a8c.PNG"
+  },
+  {
+    "revision": "316bfe684951c7997f3cb0244482374e",
+    "url": "/home/static/media/mb1.316bfe68.png"
+  },
+  {
+    "revision": "096bb49f4e038447fb59054d4f2557d1",
+    "url": "/home/static/media/mb2.096bb49f.png"
+  },
+  {
+    "revision": "dd77f30ad46484045747f5affbaa4216",
+    "url": "/home/static/media/mb3.dd77f30a.png"
+  },
+  {
+    "revision": "d5010bba076bac84edaeeddad816f476",
+    "url": "/home/static/media/mm1.d5010bba.png"
+  },
+  {
+    "revision": "520075693c60fd5ac08823f24802f9dc",
+    "url": "/home/static/media/osu.52007569.PNG"
+  },
+  {
+    "revision": "5cb398dfb0661d6b8db19ea81d52fa3f",
+    "url": "/home/static/media/ph1.5cb398df.jpg"
+  },
+  {
+    "revision": "86d930c371299ddb81ef30ceecc67947",
+    "url": "/home/static/media/ph2.86d930c3.png"
+  },
+  {
+    "revision": "3b8d9c940a349be8ae4504dc0b175846",
+    "url": "/home/static/media/ships.3b8d9c94.png"
+  },
+  {
+    "revision": "14c86f325501992c6dbbd875e324898c",
+    "url": "/home/static/media/whattowear.14c86f32.png"
+  }
+]);
